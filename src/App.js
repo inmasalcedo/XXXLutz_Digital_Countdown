54dd45ebdf3643c26e8ddeadf7a16f091c1552ab
inmasalcedo <inma.salcedo.gonzalez@gmail.com>
2019-04-08 18:05:54 +0200

    change css into scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <h1>Countdown</h1>
         </header>
+        <main>
+
+        </main>
       </div>
     );
   }
